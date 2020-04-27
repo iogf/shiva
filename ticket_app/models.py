c37de6f6bc5b544ff590195353ddfbabfb4a5d92
Iury O. G. Figueiredo <last.src@gmail.com>
2020-04-27 20:47:00 +0000

    Adding fields.

--- ticket_app/models.py
+++ ticket_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class TicketMixin(models.Model):
+    class Meta:
+        abstract = True
+
+class Ticket(TicketMixin):
+    enabled = models.BooleanField(default=False)
