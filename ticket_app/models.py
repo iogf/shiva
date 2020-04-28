6548acfe9d398801f2155703b2b523cf4190e279
Iury O. G. Figueiredo <last.src@gmail.com>
2020-04-28 13:16:42 +0000

    Adding basic fields.

--- ticket_app/models.py
+++ ticket_app/models.py
@@ -6,4 +6,11 @@ class TicketMixin(models.Model):
         abstract = True
 
 class Ticket(TicketMixin):
+    name = models.CharField(null=True,
+    blank=False, verbose_name='Name', 
+    help_text='Name', max_length=256)
+
+    email = models.EmailField(max_length=70, 
+    null=True, blank=False, unique=True)
+
     enabled = models.BooleanField(default=False)
