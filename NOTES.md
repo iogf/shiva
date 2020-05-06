9eabdd67891f429db4baa6830c3303a5df7b6186
Iury O. G. Figueiredo <last.src@gmail.com>
2020-05-06 03:20:20 +0000

    Adding basic stuff.

---
+++ NOTES.md
@@ -0,0 +1,4 @@
+
+### Notifications
+
+https://www.digitalocean.com/community/tutorials/how-to-send-web-push-notifications-from-django-applications
--- ticket_app/forms.py
+++ ticket_app/forms.py
@@ -1,14 +1,21 @@
 from django import forms
+from .models import Ticket
 from . import models
 
 class TicketForm(forms.ModelForm):
     class Meta:
-        model   = models.Ticket
+        model   = Ticket
         exclude = ('enabled', )
 
 class FindTicketForm(forms.Form):
     name = forms.CharField(required=False, 
     help_text='Example: Tau')
+
+    type = forms.ChoiceField(required=True, 
+    choices=Ticket.TYPE_CHOICES)
+
+    item = forms.ChoiceField(required=True, 
+    choices=Ticket.ITEM_CHOICES)
 
     email = forms.EmailField(required=False, 
     help_text='Example: last.src@gmail.com')
