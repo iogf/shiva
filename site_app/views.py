cd73a68340eedb2579590eef9129a72b345c5f85
Iury O. G. Figueiredo <last.src@gmail.com>
2020-05-18 11:12:26 +0000

    Adding enabled=True criteria to count existing tickets.

--- site_app/views.py
+++ site_app/views.py
@@ -5,6 +5,8 @@ from ticket_app.models import Ticket
 # Create your views here.
 class Index(View):
     def get(self, request):
-        count = Ticket.objects.count()
+        records = Ticket.objects.filter(enabled=True)
+        count = records.count()
+
         return render(request, 
             'site_app/index.html', {'count': count})
